2748b6a477ee88fac5540db5b4464af8afd284c2
TheMcnafaha <jafetguzmanflores@gmail.com>
2023-09-25 18:33:01 -0400

    added flowbite nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite/**/*.js", // add this line
+  ],
   theme: {
     extend: {
       colors: {
@@ -25,5 +28,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin"), // add this line
+  ],
 };
